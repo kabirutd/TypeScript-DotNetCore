6fdd0a3d92769abf8fe32fbd00c5d475a6244f70
kabirutd <mohammadkabir@gmail.com>
2019-10-02 14:35:41 -0500

    Add project files.

---
+++ TypeScriptProject1/wwwroot/ts/tests.ts
@@ -0,0 +1,7 @@
+﻿var firstName: string = "Mohammad";
+var surName: string = "Kabir";
+
+function fullName(x: string, y: string, spaces: number): string {
+    return x + Array(spaces + 1).join(' ') + y;
+}
+alert(fullName(firstName, surName, 3) + " Hello");
